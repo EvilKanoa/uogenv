28ae08d465e099dce1bd9bc4dd06978323cac138
EvilKanoa <kanoa@kanoa.ca>
2019-01-19 13:24:46 -0500

    Fixed perms

--- script/user.sh
+++ script/user.sh
@@ -8,6 +8,7 @@ echo "PATH=\"/usr/local/bin:/usr/bin:/bin:/usr/sbin\"" >> /home/student/.profile
 echo "student ALL=NOPASSWD:/bin/rm" >> /etc/sudoers
 echo "student ALL=NOPASSWD:/bin/mkdir" >> /etc/sudoers
 echo "student ALL=NOPASSWD:/bin/chown" >> /etc/sudoers
+echo "student ALL=NOPASSWD:/bin/su" >> /etc/sudoers
 
 exec "$@"
 
